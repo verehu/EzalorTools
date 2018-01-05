9c247147a0fea9ff166f7838a7faad0215806a69
huwei <huwei@gionee.com>
2018-01-05 10:21:35 +0800

    完成导出html功能

--- ezalor.py
+++ ezalor.py
@@ -10,41 +10,69 @@ The core function is to export data what ezalor is record.
 import os
 import sys, getopt
 import sqlite3
-import pandas as pd
+import webbrowser
 
-tableheaders = {"filepath", "process", "thread", "readcount", "readbytes", "readtime",
-                "writecount", "writebytes", "writetime", "stacktrace", "opentime", "closetime"}
+import pandas as pd
+import numpy as np
+
+tableheaders = ["filepath", "process", "thread", "readcount", "readbytes", "readtime",
+                "writecount", "writebytes", "writetime", "stacktrace", "opentime", "closetime"]
 
 def printHelpAndExit():
-    print("This is a python script for the ezalor tools which is used to io monitor.\n"
-          "You can use the script to open or off the switch, or point the package name which you want to monitor it only.\n"
-          "The core function is to export data what ezalor is record.\n"
+    print("\n"
+          "This is a python script for the ezalor tools which is used to io monitor.You can use the script to open or \n"
+          "off the switch, or point the package name which you want to monitor it only.The core function is to export \n"
+          "data what ezalor is record.\n"
           "\n"
           "Usage : ezalor [Options] [Args]\n"
           "\n"
           "     Options:\n"
-          "         -h, --help                           :Print the message and exit\n"
-          "         -s, --switch [on][off][status]       :enable or disable the ezalor\n"
+          "         -h, --help                              :Print the message and exit\n"
+          "         -s, --switch [on][off][status]          :enable or disable the ezalor\n"
+          "         -e, --export [processname] [exportpath] :export a html to the path\n"
+          "\n"
+          "     Examples:\n"
+          "         ezalor -s on                            switch on the ezalor\n"
+          "         ezalor -e com.android.gallery2          export html\n"
           )
     sys.exit(0)
+
+def wrtieToFile(path, content):
+    if ("." == path):
+        htmlPath = "export.html"
+    else:
+        htmlPath = path + "export.html"
+
+    fo = open(htmlPath, "w")
+    fo.write(content)
+    fo.close()
+    return htmlPath
 
 def convert2html(tableHeaders, result):
     d = {}
     index = 0
     for t in tableHeaders:
-        d[t] = tableHeaders[index]
+        d[t] = result[index]
         index = index + 1
     df = pd.DataFrame(d)
+    df = df[tableHeaders]
     h = df.to_html(index=False)
     return h
 
 def export(processname, path):
     os.system("adb pull /sdcard/ezalor/" + processname + "/ezalor.db" + "     " + path)
 
-    cursor = getCursor(path + "/ezalor.db")
+    cursor = getCursor("ezalor.db")
+    cursor.execute("select * from iohistory")
     results = cursor.fetchall()
 
-    print(results)
+    htmlPath = wrtieToFile(path, convert2html(tableheaders, transpose(results)))
+    print("html has exported to " + htmlPath)
+
+    webbrowser.open(htmlPath)
+
+def transpose(list_):
+    return [list(i) for i in np.array(list_).T]
 
 #    os.system("rm " + path + "ezalor.db")
 def getCursor(dbpath):
@@ -67,10 +95,6 @@ def main(argv):
         printHelpAndExit()
         return
 
-    isExport = False
-    processname = ""
-    outPath = "."
-
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             printHelpAndExit()
@@ -86,19 +110,13 @@ def main(argv):
             else:
                 printHelpAndExit()
         elif opt in ("-e", "--export"):
-            isExport = True
+            if (len(arg) == 0):
+                printHelpAndExit()
+
             processname = arg
-        elif opt in ("-o", "--output"):
-            if isExport:
-                outPath = arg
+            outPath = "." if len(args) == 0 else args[0]
 
-    if isExport:
-        if "" == processname:
-            printHelpAndExit()
-        else:
             export(processname, outPath)
-
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
